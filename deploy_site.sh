72b7d1c32282430b44ba8b86ae1eb5c33739ae6b
halexand <harriet.xander@gmail.com>
2018-11-02 15:47:58 -0400

    added deployment script

---
+++ deploy_site.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
+
+echo "Make sure to add and commit to the source files!!!!"
