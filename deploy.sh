76d82576a53864e5eab2b3a4674d594e4410db17
Orion Weller <orionw@qualtrics.com>
2019-07-19 08:24:48 -0600

    update repo and deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cp -r public/	orionweller.github.io/
+cd orionweller.github.io
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
