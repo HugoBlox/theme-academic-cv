a63d57bff7ac0d3a5a705eaeaa83b427a8291dea
Adriano Fantini <adr.fantini@gmail.com>
2019-10-13 13:09:41 +0200

    Fix travis publish script

--- travis/publish.sh
+++ travis/publish.sh
@@ -6,15 +6,15 @@ echo '######################################'
 
 echo '---- Cloning repo into /tmp ----'
 cd /tmp
-git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} gh-pages 2>&1
-cd gh-pages
+git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} gbpn 2>&1
+cd gbpn
 
-echo '---- Switch to gh-pages branch ----'
-git checkout gh-pages
+#echo '---- Switch to gh-pages branch ----'
+#git checkout gh-pages
 
-echo '---- Copy latest PDF build ----'
-mkdir -p dist
-cp $RESULT_PDF_PATH ./dist/${TRAVIS_BRANCH}-thesis.pdf
+#echo '---- Copy latest PDF build ----'
+#mkdir -p dist
+#cp $RESULT_PDF_PATH ./dist/${TRAVIS_BRANCH}-thesis.pdf
 
 echo '---- Set git settings ----'
 git config --global user.name $GIT_AUTHOR_NAME
@@ -22,8 +22,8 @@ git config --global user.email $GIT_AUTHOR_EMAIL
 git config --global push.default matching
 
 echo '---- Add files, commit and push ----'
-git add -A
-git commit -m "Adding latest build of pdf to gh-pages"
+git add $RESULT_PDF_PATH
+git commit -m "Adding latest build of pdf compiled by Travis"
 git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
 
 echo '######################################'
