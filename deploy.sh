4755f7c9aaeb599db6e57823aa2d15cd2f2076a9
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2020-04-20 00:19:47 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-hugo
-rsync  -avz --delete public/ tiagoboldt.net:~/www/tiagoboldt.net/
+backup(){
+  if [[ $1 = "dry-run" ]]
+  then
+    echo "dry run"
+    rsync -avv -n --ignore-existing --recursive --delete ./public/ tiagoboldt.net:~/www/tiagoboldt.net/
+    return 1
+  fi
+  echo "real deal"
+  rsync -avv -n --ignore-existing --recursive --delete ./public/ tiagoboldt.net:~/www/tiagoboldt.net/
+}
+
+hugo --cleanDestinationDir --minify
+
+backup "dry-run"
+read -p "Continue? [yN]? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "canceled"
+    exit 1
+fi
+backup
