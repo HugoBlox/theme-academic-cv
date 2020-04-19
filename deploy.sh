628e746cf3e13c5a039ca8ddc91027de80aa3ba2
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2020-04-20 00:43:49 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,18 @@ backup(){
   if [[ $1 = "dry-run" ]]
   then
     echo "dry run"
-    rsync -avv -n --ignore-existing --recursive --delete ./public/ tiagoboldt.net:~/www/tiagoboldt.net/
+    rsync -avvu -n --recursive --delete ./public/ tiagoboldt.net:~/www/tiagoboldt.net/
     return 1
   fi
   echo "real deal"
-  rsync -avv -n --ignore-existing --recursive --delete ./public/ tiagoboldt.net:~/www/tiagoboldt.net/
+  rsync -avvu --recursive --delete ./public/ tiagoboldt.net:~/www/tiagoboldt.net/
 }
 
 hugo --cleanDestinationDir --minify
 
 backup "dry-run"
 read -p "Continue? [yN]? " -n 1 -r
+echo ""
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     echo "canceled"
