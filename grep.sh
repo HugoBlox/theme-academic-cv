333e075c3fe6d3fc00864bd9ae707cdec1d1b7fb
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-04-29 11:50:36 +0900

    grep.sh作成
    ・「themes/academic/exampleSite」と「docs/」内以外のファイルをgrep検索するshellスクリプト作成

--- grep.sh
+++ grep.sh
@@ -1,15 +1,38 @@
 echo "検索する文字列を入力してください"
 read target
-array=($(grep -r $target *))
+grep -n -r $target * | sed -e '/^themes\/academic\/exampleSite/d'| sed -e '/^docs/d' > grep_result.txt
+#arr=($(grep -r $target *))    
+#echo ${arr[0]}
+#s=""
+#for item in ${arr[@]}
+#do
+#  s=$s$item
+#  if [ ${var:0:28}!="themes/academic/exampleSite/" ]; then
+#    if [ ${var:0:5}]!="docs" ]; then
+#      echo $var >> result.txt
+#    fi
+#  fi
+#done
 
-for var in ${array[@]}
-do
-  if [ ${var:0:28}!="themes/academic/exampleSite/" ]; then
-    if [ ${var:0:5}]!="docs" ]; then
-      echo $var
-    fi
-  fi
-done
-echo ""
-echo ""
-read -p "終了するにはなにかキーを入力してください"
+#echo $s
+#for ((i = 0; i < ${#arr[@]}; i++))
+#do
+#  var=${arr[$i]}
+#  if [ ${var:0:28}!="themes/academic/exampleSite/" ]; then
+#    if [ ${var:0:5}]!="docs" ]; then
+#      echo $var >> result.txt
+#    fi
+#  fi
+#done
+#for var in ${arr[@]}
+#do
+#  if [ ${var:0:28}!="themes/academic/exampleSite/" ]; then
+#    if [ ${var:0:5}]!="docs" ]; then
+#      echo $var >> result.txt
+#    fi
+#  fi
+#done
+#grep -r $target *
+#echo ""
+#echo ""
+#read -p "終了するにはなにかキーを入力してください"
