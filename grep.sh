2eb11d6caeb044d3fded5d55fed694fc1d346a6a
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-04-29 11:53:42 +0900

    grep.shの不要部分削除

--- grep.sh
+++ grep.sh
@@ -1,38 +1,4 @@
 echo "検索する文字列を入力してください"
 read target
 grep -n -r $target * | sed -e '/^themes\/academic\/exampleSite/d'| sed -e '/^docs/d' > grep_result.txt
-#arr=($(grep -r $target *))    
-#echo ${arr[0]}
-#s=""
-#for item in ${arr[@]}
-#do
-#  s=$s$item
-#  if [ ${var:0:28}!="themes/academic/exampleSite/" ]; then
-#    if [ ${var:0:5}]!="docs" ]; then
-#      echo $var >> result.txt
-#    fi
-#  fi
-#done
 
-#echo $s
-#for ((i = 0; i < ${#arr[@]}; i++))
-#do
-#  var=${arr[$i]}
-#  if [ ${var:0:28}!="themes/academic/exampleSite/" ]; then
-#    if [ ${var:0:5}]!="docs" ]; then
-#      echo $var >> result.txt
-#    fi
-#  fi
-#done
-#for var in ${arr[@]}
-#do
-#  if [ ${var:0:28}!="themes/academic/exampleSite/" ]; then
-#    if [ ${var:0:5}]!="docs" ]; then
-#      echo $var >> result.txt
-#    fi
-#  fi
-#done
-#grep -r $target *
-#echo ""
-#echo ""
-#read -p "終了するにはなにかキーを入力してください"
