ad042cf376a81203cdd0a424b74723ab7dc2a6c9
Juris710 <50994088+Juris710@users.noreply.github.com>
2020-04-29 12:34:11 +0900

    grep.shの結果出力先を変更

--- grep.sh
+++ grep.sh
@@ -1,4 +1,4 @@
 echo "検索する文字列を入力してください"
 read target
-grep -n -r $target * | sed -e '/^themes\/academic\/exampleSite/d'| sed -e '/^docs/d' > grep_result.txt
+grep -n -r $target * | sed -e '/^themes\/academic\/exampleSite/d'| sed -e '/^docs/d' > "grep_result/${target}.txt"
 
