5cc017a8b655bfdc9e04e676997c2c842495b313
Gunnar Voet <gvoet@ucsd.edu>
2020-06-07 15:55:19 -0700

    udpate cv

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,8 @@ rsync -av --delete --exclude ".git" public/ deployment
 cd deployment
 git add -A
 # we need the || true, as sometimes you do not have any content changes
-# and git woundn't commit and you don't want to break the CI because of that
+# where git does not want to commit and you don't want to break the CI because
+# of that
 git commit -m "rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
 git push
 
