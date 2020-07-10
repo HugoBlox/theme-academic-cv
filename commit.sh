4e087afc9cb134151d86ada3291a61d6219511f8
Juris <50994088+Juris710@users.noreply.github.com>
2020-07-10 10:28:36 +0900

    commit.sh改善
    master以外のbranchでcommit仕様とした場合、確認を求めるように

--- commit.sh
+++ commit.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+branch_name=`git symbolic-ref --short HEAD`
+if [ $branch_name != "master" ]; then
+  echo "現在のbranchはmasterではありません。. "
+  echo -n "Commitを続行しますか?(y/n):"
+  read answer
+  if [ ${answer^^} != "Y" ]; then
+    exit
+  fi
+fi
 hugo
 git add .
 git commit
