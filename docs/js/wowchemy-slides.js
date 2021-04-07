7edb93668cd263befffa4e6ad7d67a6746ec2ea1
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-04-08 00:05:41 +0900

    微修正

--- docs/js/wowchemy-slides.js
+++ docs/js/wowchemy-slides.js
@@ -2,7 +2,6 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
