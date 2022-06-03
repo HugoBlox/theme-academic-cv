71f3f810e3440d78ad751ad2687abee06b1ffca9
Sophie <sophie.fortz@unamur.be>
2022-06-03 20:01:22 +0200

    Test deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 hugo
-( cd public && git add . && git commit -m "Build website" && git push origin master )
+( cd public && git add . && git commit -m "Build website" && git push origin main )
